db0f2bf3ab45645d3fdeefaf356c9b63f9129c4e
Pardo-Dev-78 <car.pardo@duocuc.cl>
2024-06-21 18:29:57 -0400

    TEST: validar __str__

--- web_ferremas/tests.py
+++ web_ferremas/tests.py
@@ -9,8 +9,22 @@ def test_crear_categoria_producto():
     assert categoria_producto.categoria == "Gasfiteria"
 
 @pytest.mark.django_db
+def test_str_categoria_producto():
+    categoria_producto = CategoriaProducto.objects.create(
+        categoria="Gasfiteria"
+    )
+    assert str(categoria_producto) == "Gasfiteria"
+
+@pytest.mark.django_db
 def test_crear_tipo_producto():
     tipo_producto = TipoProducto.objects.create(
         tipo = "Lubricante"
     )
     assert tipo_producto.tipo == "Lubricante"
+
+@pytest.mark.django_db
+def test_crear_tipo_producto():
+    tipo_producto = TipoProducto.objects.create(
+        tipo = "Lubricante"
+    )
+    assert str(tipo_producto) == "Lubricante"

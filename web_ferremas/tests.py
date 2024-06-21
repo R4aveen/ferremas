894bb4c66aee8f76dcbcdb0eaa147b2a78f4b40d
Pardo-Dev-78 <car.pardo@duocuc.cl>
2024-06-21 19:00:15 -0400

    TEST: Producto

--- web_ferremas/tests.py
+++ web_ferremas/tests.py
@@ -28,3 +28,29 @@ def test_crear_tipo_producto():
         tipo = "Lubricante"
     )
     assert str(tipo_producto) == "Lubricante"
+
+@pytest.mark.django_db
+def test_crear_producto():
+    categoria_producto = CategoriaProducto.objects.create(
+        categoria="Gasfiteria"
+    )
+    tipo_producto = TipoProducto.objects.create(
+        tipo = "Lubricante"
+    )
+
+    producto = Producto.objects.create(
+        nombre = "Cierra electrica",
+        precio = 5990,
+        stock = 55,
+        descripcion = "Cierra de buena calidad",
+        en_oferta = False
+    )
+    producto.categoria.set({categoria_producto})
+    producto.tipo.set({tipo_producto})
+    assert producto.nombre == "Cierra electrica"
+    assert producto.precio == 5990
+    assert producto.stock == 55
+    assert producto.descripcion == "Cierra de buena calidad"
+    assert categoria_producto in producto.categoria.all()
+    assert tipo_producto in producto.tipo.all()
+    assert producto.en_oferta == False
